c79bec4195cc02687e4a763e9c5c169598df550a
turibiusrozario <80226974+turibiusrozario@users.noreply.github.com>
2026-01-04 23:50:54 +0000

    Deploying to gh-pages from @ turibiusrozario/turibiusrozario.github.io@afcf6f65fa461f61d260634ec10e18e80a1c4ee1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -522,7 +522,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("./feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
